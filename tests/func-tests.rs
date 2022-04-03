263cdd71674e7a5499c3f8aa42bef204dbe261af
Toshihiro Kamiya <kamiya@mbj.nifty.com>
2022-04-03 17:08:28 +0900

    tests: add an unittest

---
+++ tests/func-tests.rs
@@ -0,0 +1,19 @@
+#[cfg(test)]
+#[allow(non_snake_case)]
+
+#[cfg(test)]
+mod test {
+    use o_o::*;
+
+    #[test]
+    fn command_exists_for_ls() {
+        let ls_command_exists = command_exists("ls");
+        assert!(ls_command_exists);
+    }
+
+    #[test]
+    fn command_exists_for_hoge4() {
+        let h4_command_exists = command_exists("hoge-hoge-hoge-hoge");
+        assert!(!h4_command_exists);
+    }
+}
